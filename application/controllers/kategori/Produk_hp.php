8729bb5a185914d2a215ba6a0f14f937c8e758d7
boycakra <boycakra69@gmail.com>
2019-11-29 22:24:26 +0700

    Produk detail HP & Amd,Asset gambar

--- application/controllers/kategori/Produk_hp.php
+++ application/controllers/kategori/Produk_hp.php
@@ -13,5 +13,18 @@
         $this->load->view('kategori/produkhp' , $list);
         $this->load->view('partial/footer');
     }
+    public function subDetail($id_brg){
+
+        $data['judul'] = 'Halaman HP';
+        $list['barang']= $this->model_hp->get_data($id_brg);
+
+        $this->load->view('partial/navbar');
+        $this->load->view('partial/topbar');
+        $this->load->view('partial/header', $data);
+        $this->load->view('produk/shop_detail', $list);
+        $this->load->view('partial/footer');
+        
+        
+    }
 }
 ?>
--- application/models/model_amd.php
+++ application/models/model_amd.php
@@ -6,5 +6,10 @@ class Model_amd extends CI_model{
 
         return $this->db->get('tabel_amd');
     }
+    public function get_data($id_brg){
+
+        $query = "SELECT * FROM tabel_amd WHERE id_barang = '".$id_brg."'";
+        return $this->db->query($query)->row();
+    }
 }
 ?>
